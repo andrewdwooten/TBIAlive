a2cb60c0bee235d9ae32c36a418eefb589bb22de
Andy Wooten <andrewdwooten@icloud.com>
2017-08-21 11:18:42 -0600

    Complete materialize config; add tbi_logo to public dir, add and style tbi logo on landing page; add test to confirm presence of logo

--- spec/features/guest_visits_landing_page_spec.rb
+++ spec/features/guest_visits_landing_page_spec.rb
@@ -5,4 +5,11 @@ feature 'user visits landing page' do
     visit '/'
     expect(current_path).to eq('/')
   end
+  scenario 'and they see the tbi_alive_logo' do
+    visit '/'
+    within '#logo_box' do
+      expect(page).to have_selector('#tbi_landing_logo')
+      page.find('#tbi_landing_logo')['src'].should have_content 'tbi_logo.png'
+    end
+  end
 end
