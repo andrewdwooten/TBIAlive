382799d8c8c5c38fd35339a095cf2d194f8a681b
Andy Wooten <andrewdwooten@icloud.com>
2017-08-21 15:57:45 -0600

    Add test to confirm presence of #philosophy-card

--- spec/features/guest_visits_landing_page_spec.rb
+++ spec/features/guest_visits_landing_page_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 feature 'user visits landing page' do
-  scenario 'and is on the root path' do
+  before(:each) do
     visit '/'
+  end
+  scenario 'and is on the root path' do
     expect(current_path).to eq('/')
   end
   scenario 'and they see the tbi_alive_logo' do
-    visit '/'
     within '.navbar' do
       expect(page).to have_selector('#tbi-navbar-logo')
       page.find('#tbi-navbar-logo')['src'].should have_content 'tbi_logo.png'
     end
   end
   scenario 'and they see the navbar' do
-    visit '/'
     expect(page).to have_selector('.navbar')
+  end
+  scenario 'and they see the philosophy card' do
+    expect(page).to have_content('#philosophy-card')
   end
 end
