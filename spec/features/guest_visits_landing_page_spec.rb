e5a3687fbe9266242689ae1ee6f9564db75331fe
Andy Wooten <andrewdwooten@icloud.com>
2017-08-23 10:14:24 -0600

    Completes layout of navigation bar and philosophy card; all accessibility violations resolved.

--- spec/features/guest_visits_landing_page_spec.rb
+++ spec/features/guest_visits_landing_page_spec.rb
@@ -5,18 +5,22 @@ feature 'user visits landing page' do
     visit '/'
   end
   scenario 'and is on the root path' do
+    
     expect(current_path).to eq('/')
   end
   scenario 'and they see the tbi_alive_logo' do
+    
     within '.navbar' do
       expect(page).to have_selector('#tbi-navbar-logo')
       page.find('#tbi-navbar-logo')['src'].should have_content 'tbi_logo.png'
     end
   end
   scenario 'and they see the navbar' do
+    
     expect(page).to have_selector('.navbar')
   end
   scenario 'and they see the philosophy card' do
+    
     expect(page).to have_content('#philosophy-card')
   end
 end
